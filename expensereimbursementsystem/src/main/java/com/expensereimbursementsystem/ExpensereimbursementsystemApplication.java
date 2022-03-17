199db0641f41ca3f2bc5b92fb51341def70d74c9
Adityagithub24 <adityakpurohit.mds@gmail.com>
2022-03-17 10:44:08 +0530

    Folder Structure Created

---
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/ExpensereimbursementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.expensereimbursementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensereimbursementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensereimbursementsystemApplication.class, args);
+	}
+
+}
