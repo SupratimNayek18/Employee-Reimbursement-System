442083d69672adb28de7f1d4b9c5823fab8225a8
SupratimNayek18 <supratimnayek@gmail.com>
2022-03-18 10:26:57 +0530

    Employee Entity Version 1

--- expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Employee.java
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Employee.java
@@ -1,5 +1,81 @@
 package com.expensereimbursementsystem.entities;
 
 public class Employee {
+	
+	private Integer id;
+	
+	
+	private String name;
+	
+	
+	private String email;
+	
+	
+	private String role;
 
+
+	// Default Constructor
+	
+	public Employee() {}
+
+	
+	//Parameterized Constructors
+	
+	public Employee(String name, String email, String role) {
+		this.name = name;
+		this.email = email;
+		this.role = role;
+	}
+	
+
+	//Getters and Setters
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	//toString method
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", role=" + role + "]";
+	}
+	
+	
 }
