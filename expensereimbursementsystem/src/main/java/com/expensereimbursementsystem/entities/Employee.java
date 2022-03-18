1deb0b717c0e9c9b046992f2622edd7ab7120444
Adityagithub24 <adityakpurohit.mds@gmail.com>
2022-03-18 11:04:21 +0530

    Anotated Employee Entity Version 2

--- expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Employee.java
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Employee.java
@@ -1,16 +1,28 @@
 package com.expensereimbursementsystem.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee_Table")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "EmployeeId")
 	private Integer id;
 	
-	
+	@Column(name = "EmployeeName")
 	private String name;
 	
-	
+	@Column(name="EmployeeEmail")
 	private String email;
 	
-	
+	@Column(name="EmployeeRole")
 	private String role;
 
 
