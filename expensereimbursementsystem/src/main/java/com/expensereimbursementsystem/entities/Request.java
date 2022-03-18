f08667444ff2944f4ecfb0ba1914b286f43334c6
Aman Singh <Amansinghpatel429@gmail.com>
2022-03-18 11:45:52 +0530

    request update

--- expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Request.java
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/entities/Request.java
@@ -1,5 +1,7 @@
 package com.expensereimbursementsystem.entities;
 
+import java.util.Locale;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,17 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Employee_Table")
 public class Request {
+
+
+	public Request(Integer id, String description, String status, Locale requestDate, Locale approval, double amount) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.status = status;
+		this.requestDate = requestDate;
+		this.approval = approval;
+		Amount = amount;
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,15 +36,73 @@ public class Request {
 	private String status;
 	
 	@Column(name="RequestDate")
-	private String requestDate;
+	private Locale requestDate;
 	
-	@Column(name="private String email;")
-	 private String email;
+	@Column(name="ApprovalDate;")
+	private Locale approval;
 	
 	@Column(name="Amount")
-	private String Amount;
+	private double Amount;
 	
-	@Column(name="EmployeeId")
-	private String EmployeeId;
+	public Request() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Locale getRequestDate() {
+		return requestDate;
+	}
+
+	public void setRequestDate(Locale requestDate) {
+		this.requestDate = requestDate;
+	}
+
+	public Locale getApproval() {
+		return approval;
+	}
+
+	public void setApproval(Locale approval) {
+		this.approval = approval;
+	}
+
+	public double getAmount() {
+		return Amount;
+	}
+
+	public void setAmount(double amount) {
+		Amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Request [id=" + id + ", description=" + description + ", status=" + status + ", requestDate="
+				+ requestDate + ", approval=" + approval + ", Amount=" + Amount + "]";
+	}
+	
+
 }
 
