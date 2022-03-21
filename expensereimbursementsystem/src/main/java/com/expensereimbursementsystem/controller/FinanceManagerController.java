9cb82c23419c7a0594eec41469ff505d7c234aa4
Adityagithub24 <adityakpurohit.mds@gmail.com>
2022-03-21 15:39:44 +0530

    financemanager controller completed

--- expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/controller/FinanceManagerController.java
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/controller/FinanceManagerController.java
@@ -1,5 +1,65 @@
 package com.expensereimbursementsystem.controller;
 
-public class FinanceManagerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.expensereimbursementsystem.entities.Employee;
+import com.expensereimbursementsystem.entities.ExpenseDetails;
+import com.expensereimbursementsystem.exceptions.AccessDeniedException;
+import com.expensereimbursementsystem.exceptions.EmployeeNotFoundException;
+import com.expensereimbursementsystem.exceptions.ExpenseNotFoundException;
+import com.expensereimbursementsystem.services.EmployeeService;
+import com.expensereimbursementsystem.services.ExpenseDetailsService;
+
+@RestController
+@RequestMapping("finance_manager")
+public class FinanceManagerController {
+	
+	@Autowired
+	ExpenseDetailsService expensedetailsService;
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@GetMapping("/fetchallemployees")
+	public ResponseEntity<List<Employee>> fetchAllEmployees() throws EmployeeNotFoundException
+	{
+		return new ResponseEntity<List<Employee>>(employeeService.viewAllEmployee(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/fetchbyexpenseid/{id}")
+	public ResponseEntity<ExpenseDetails> fetchByExpenseId(@PathVariable (value="id") Integer id) throws ExpenseNotFoundException
+	{
+		return new ResponseEntity<ExpenseDetails>(expensedetailsService.fetchById(id),HttpStatus.OK);
+	}
+	
+	@GetMapping("/fetchbyemployeeid/{id}")
+	public ResponseEntity<List<ExpenseDetails>> fetchByEmployeeId(@PathVariable (value="id") Integer id) throws EmployeeNotFoundException
+	{
+		return new ResponseEntity<List<ExpenseDetails>>(expensedetailsService.fetchByEmployeeId(id),HttpStatus.OK);
+	}
+	
+	@GetMapping("/fetchallrequests/{id}")
+	public ResponseEntity<List<ExpenseDetails>> fetchAllRequests(@PathVariable (value="id") Integer id) throws ExpenseNotFoundException, EmployeeNotFoundException, AccessDeniedException
+	{
+		return new ResponseEntity<List<ExpenseDetails>>(expensedetailsService.fetchAll(id),HttpStatus.OK);
+	}
+	
+	@PutMapping("/updatestatusofrequest")
+	public ResponseEntity<String>updateRequestStatus(@RequestParam Integer employeeId ,@RequestParam String status ,@RequestParam Integer id) throws EmployeeNotFoundException, AccessDeniedException
+	{
+		return new ResponseEntity<String>(expensedetailsService.updateExpense(employeeId, status, id),HttpStatus.OK);
+	}
+	
 }
+
+
