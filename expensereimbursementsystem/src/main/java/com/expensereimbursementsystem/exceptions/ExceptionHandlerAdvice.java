a4dbebd1c5aa271df87131a2279622de2bdfc269
SupratimNayek18 <supratimnayek@gmail.com>
2022-03-22 12:56:15 +0530

    Exceptions Advice Done

--- expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/exceptions/ExceptionHandlerAdvice.java
+++ expensereimbursementsystem/src/main/java/com/expensereimbursementsystem/exceptions/ExceptionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.expensereimbursementsystem.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,9 +9,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandlerAdvice {
 	
 	@ExceptionHandler(EmployeeNotFoundException.class)
-	public ResponseEntity<String> employeeNotFound()
+	public ResponseEntity<String> employeeNotFound(EmployeeNotFoundException enfe)
 	{
-		
+		return  new ResponseEntity<>(enfe.getMessage(),HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<String> accessDenied(AccessDeniedException ade)
+	{
+		return  new ResponseEntity<>(ade.getMessage(),HttpStatus.FORBIDDEN);
+	}
+	
+	@ExceptionHandler(ExpenseNotFoundException.class)
+	public ResponseEntity<String> expenseNotFound(ExpenseNotFoundException exnfe)
+	{
+		return  new ResponseEntity<>(exnfe.getMessage(),HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(DeleteEmployeeException.class)
+	public ResponseEntity<String> deleteEmployee(DeleteEmployeeException dex)
+	{
+		return  new ResponseEntity<>(dex.getMessage(),HttpStatus.NOT_FOUND);
 	}
 	
 	
