7cb803edc3d73d077640d81c578596b09282e40a
SupratimNayek18 <supratimnayek@gmail.com>
2022-04-21 10:34:30 +0530

    Docker-Compose File created

--- expensereimbursementsystem/src/test/java/com/expensereimbursementsystem/EmployeeControllerTest.java
+++ expensereimbursementsystem/src/test/java/com/expensereimbursementsystem/EmployeeControllerTest.java
@@ -1,72 +1,72 @@
-package com.expensereimbursementsystem;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-
-@SpringBootTest()
-@AutoConfigureMockMvc
-class EmployeeControllerTest {
-	
-	@Autowired
-	MockMvc mvc;
-
-	//test for get mapping for fetching employee by id
-	@Test
-	void testByEmployeeById() {
-		
-		try {
-			mvc.perform(get("/employee/getEmployee/1"))
-			  .andExpect(status().isOk())
-			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	//TODO test for get mapping for fetching expense by id
-	@Test
-	void testByExpenseId() {
-		
-		try {
-			mvc.perform(get("/employee/getByExpenseId/1"))
-			  .andExpect(status().isOk())
-			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
-
-	//TODO test for viewing expense details by employee id
-	
-	@Test
-	void testExpenseByEmployeeId() {
-		
-		try {
-			mvc.perform(get("/employee/getAllExpenses/1"))
-			  .andExpect(status().isOk())
-			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-
-}
-
-
-
-
+//package com.expensereimbursementsystem;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//
+//@SpringBootTest()
+//@AutoConfigureMockMvc
+//class EmployeeControllerTest {
+//	
+//	@Autowired
+//	MockMvc mvc;
+//
+//	//test for get mapping for fetching employee by id
+//	@Test
+//	void testByEmployeeById() {
+//		
+//		try {
+//			mvc.perform(get("/employee/getEmployee/1"))
+//			  .andExpect(status().isOk())
+//			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//	//TODO test for get mapping for fetching expense by id
+//	@Test
+//	void testByExpenseId() {
+//		
+//		try {
+//			mvc.perform(get("/employee/getByExpenseId/1"))
+//			  .andExpect(status().isOk())
+//			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//	
+//
+//	//TODO test for viewing expense details by employee id
+//	
+//	@Test
+//	void testExpenseByEmployeeId() {
+//		
+//		try {
+//			mvc.perform(get("/employee/getAllExpenses/1"))
+//			  .andExpect(status().isOk())
+//			  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//
+//}
+//
+//
+//
+//
--- expensereimbursementsystem/src/test/java/com/expensereimbursementsystem/FinanceManagerControllerTest.java
+++ expensereimbursementsystem/src/test/java/com/expensereimbursementsystem/FinanceManagerControllerTest.java
@@ -1,91 +1,91 @@
-package com.expensereimbursementsystem;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-@SpringBootTest()
-@AutoConfigureMockMvc
-class FinanceManagerControllerTest {
-
-	@Autowired
-	MockMvc mvc;
-
-	//test for get mapping for fetching all employees
-	@Test
-	void testAllEmployees() {
-
-		try {
-			mvc.perform(get("/finance_manager/fetchallemployees"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	//test for get mapping to fetch expense by id
-	@Test
-	void testByExpenseId() {
-
-		try {
-			mvc.perform(get("/finance_manager/fetchbyexpenseid/1"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-	
-	//test for get expenses by employee id
-	@Test
-	void testExpensesByEmployeeId() {
-		try {
-			mvc.perform(get("/finance_manager/fetchbyemployeeid/1"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-	
-
-	//test for fetching all requests and checking forbidden access
-	@Test
-	void testAllRequestsForbidden() {
-		try {
-			mvc.perform(get("/finance_manager/fetchallrequests/1"))
-			.andExpect(status().isForbidden());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-	
-	//test for fetching all requests and checking ok access
-	@Test
-	void testAllRequestsOk() {
-		try {
-			mvc.perform(get("/finance_manager/fetchallrequests/3"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-
-
-
-}
+//package com.expensereimbursementsystem;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@SpringBootTest()
+//@AutoConfigureMockMvc
+//class FinanceManagerControllerTest {
+//
+//	@Autowired
+//	MockMvc mvc;
+//
+//	//test for get mapping for fetching all employees
+//	@Test
+//	void testAllEmployees() {
+//
+//		try {
+//			mvc.perform(get("/finance_manager/fetchallemployees"))
+//			.andExpect(status().isOk())
+//			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//	//test for get mapping to fetch expense by id
+//	@Test
+//	void testByExpenseId() {
+//
+//		try {
+//			mvc.perform(get("/finance_manager/fetchbyexpenseid/1"))
+//			.andExpect(status().isOk())
+//			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//	
+//	//test for get expenses by employee id
+//	@Test
+//	void testExpensesByEmployeeId() {
+//		try {
+//			mvc.perform(get("/finance_manager/fetchbyemployeeid/1"))
+//			.andExpect(status().isOk())
+//			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//	
+//
+//	//test for fetching all requests and checking forbidden access
+//	@Test
+//	void testAllRequestsForbidden() {
+//		try {
+//			mvc.perform(get("/finance_manager/fetchallrequests/1"))
+//			.andExpect(status().isForbidden());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//	
+//	//test for fetching all requests and checking ok access
+//	@Test
+//	void testAllRequestsOk() {
+//		try {
+//			mvc.perform(get("/finance_manager/fetchallrequests/3"))
+//			.andExpect(status().isOk())
+//			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//
+//
+//
+//}
